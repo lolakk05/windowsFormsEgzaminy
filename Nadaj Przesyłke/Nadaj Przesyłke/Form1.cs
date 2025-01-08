7a9b2951710b762ed92631df3e70680103b02a66
karolstolarczyk <110538419+lolakk05@users.noreply.github.com>
2025-01-08 15:33:55 +0100

    Update Form1.cs

--- Nadaj Przesyłke/Nadaj Przesyłke/Form1.cs
+++ Nadaj Przesyłke/Nadaj Przesyłke/Form1.cs
@@ -24,17 +24,17 @@ namespace Nadaj_Przesyłke
 
         private void sprawdzCene_Click(object sender, EventArgs e)
         {
-            if (pocztowka.Checked)
+            if (pocztowka.Checked) // sprawdzamy czy checkbox zaznaczony
             {
-                zdjecie.Image = Properties.Resources.pocztowka;
-                cena.Text = "Cena: 1zł";
+                zdjecie.Image = Properties.Resources.pocztowka; // z jakis resourcow ustawiamy pocztowke (ustawia sie to w wlasnosciach zdjecia, dodajemy do tych risorcow wszystkie zdjecia jakie tam beda sie wyswietlaly)
+                cena.Text = "Cena: 1zł"; // updejt labela
             }
-            if (list.Checked)
+            if (list.Checked) // podobnie do 1
             {
-                zdjecie.Image = Properties.Resources.list;
+                zdjecie.Image = Properties.Resources.list; 
                 cena.Text = "Cena: 1.5zł";
-            }
-            if (paczka.Checked)
+            } 
+            if (paczka.Checked) // podobnie do 1
             {
                 zdjecie.Image = Properties.Resources.paczka;
                 cena.Text = "Cena: 10zł";
@@ -43,21 +43,20 @@ namespace Nadaj_Przesyłke
 
         private void zatwierdz_Click(object sender, EventArgs e)
         {
-            if (kod.TextLength < 5 || kod.TextLength > 5)
-            {
+            if (kod.TextLength < 5 || kod.TextLength > 5) { // sprawdzamy czy haslo ma 5 znakow jesli nie to komunikat (mozna chyba tez zrobic kod.TextLength != 5)
                 MessageBox.Show("Nieprawidłowa liczba cyfr w kodzie\r\npocztowym");
                 return;
             }
-            String cyfry = "0123456789";
-            foreach (char c in kod.Text)
+            String cyfry = "0123456789"; // to useless zapomnailem usunac
+            foreach (char c in kod.Text) // przechodzimy przez kazdy znak (c)
             {
-                if (!char.IsDigit(c))
+                if (!char.IsDigit(c)) // sprawdzamy czy jest liczba
                 {
-                    MessageBox.Show("Kod nie zawiera samych cyfr");
+                    MessageBox.Show("Kod nie zawiera samych cyfr"); // 
                     return;
                 }
             }
-            MessageBox.Show("Kod jest git");
+            MessageBox.Show("Kod jest git"); // komunikat jesli wszystko przeszlo i funkcja nie zakonczyla wczesniej swojego dzialania
         }
     }
 }
